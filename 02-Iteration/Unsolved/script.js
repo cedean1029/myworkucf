14e12d20bde552a0bc4a7f9b5c23120daa0d077f
cedean1029 <cedean1029@gmail.com>
2022-01-04 21:05:04 -0500

    jan 4 work

---
+++ 02-Iteration/Unsolved/script.js
@@ -0,0 +1,9 @@
+// TODO: Write Your JavaScript Code Here
+var students = ["Sarah", "Orlando", "Ismeal", "Hung", "Logan"];
+
+console.log(students.length)
+
+for(var i = 0; i < students.length; i++)
+{
+    console.log('Great to see you,' + students[i]. + '!');
+}
