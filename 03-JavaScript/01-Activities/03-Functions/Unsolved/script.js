9b9092f8e407b32c09034ac8d8c46edff0582b0e
cedean1029 <cedean1029@gmail.com>
2022-01-20 19:04:33 -0500

    1/18 classwork

---
+++ 03-JavaScript/01-Activities/03-Functions/Unsolved/script.js
@@ -0,0 +1,12 @@
+// TODO: Write Your JavaScript Code Here
+function isEqual (x, y) {
+    if (x===y) {
+    console.log("is Equal in value and type")
+    } else if (x==y) {
+        console.log("is Equal in value")
+    } else {
+        console.log("Not equal in value and type")
+    }
+    return;
+}
+isEqual (1, 3)
